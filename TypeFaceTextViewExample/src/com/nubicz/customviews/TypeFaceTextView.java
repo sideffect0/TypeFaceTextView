9525962becd533d52a099d867193e0d4c38b04a9
renlinx007 <mail3renith@gmail.com>
2014-10-31 22:28:45 +0530

    example project updated

--- TypeFaceTextViewExample/src/com/nubicz/customviews/TypeFaceTextView.java
+++ TypeFaceTextViewExample/src/com/nubicz/customviews/TypeFaceTextView.java
@@ -3,9 +3,13 @@ package com.nubicz.customviews;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Typeface;
+import android.os.Handler;
 import android.util.AttributeSet;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.nubicz.typefacetextview.R;
 
@@ -13,8 +17,35 @@ public class TypeFaceTextView extends TextView{
 
 	public String data = null;
 	public Boolean is_animate = false;
-	public Integer animate_delay = 500;
+	public Integer mDelay = 500;
+	private int mIndex = 0;
+	public CharSequence mText = "";
+	protected AlphaAnimation fadeIn = null; 
+	protected AlphaAnimation fadeOut = null;
+	protected AnimationSet animation;
 	
+	private Handler mHandler = new Handler();
+    private Runnable characterAdder = new Runnable() {
+        @Override
+        public void run() {
+            setText(mText.subSequence(0, mIndex++));
+            if(mIndex <= mText.length()) {
+                mHandler.postDelayed(characterAdder, mDelay);
+            }
+            if(mIndex == mText.length()){
+            	mHandler.postDelayed(animationAdder, mDelay);
+            }
+        }
+    };
+    
+    private Runnable animationAdder = new Runnable() {
+		
+		@Override
+		public void run() {
+        	setAnimation(animation);			
+		}
+	};
+    
 	public TypeFaceTextView(Context context) {
 		super(context);
 	}
@@ -32,9 +63,11 @@ public class TypeFaceTextView extends TextView{
     public void init(Context context,AttributeSet attrs){
     	
     	TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.TypeFaceTextView);
+    	
     	data = ta.getString(R.styleable.TypeFaceTextView_customFont);
     	is_animate = ta.getBoolean(R.styleable.TypeFaceTextView_typeEffect, false);
-    	animate_delay = ta.getInteger(R.styleable.TypeFaceTextView_typeEffectDelay,500);
+    	mDelay = ta.getInteger(R.styleable.TypeFaceTextView_typeEffectDelay,500);
+    	
     	if((data == null) || (data.length() == 0)){
              ta.recycle();
              return;
@@ -48,7 +81,23 @@ public class TypeFaceTextView extends TextView{
     }
     
     public void animateText(){
+    	mText = getText();
+    	mIndex = 0;
+    	setText("");
     	
+    	fadeIn = new AlphaAnimation(0.0f , 1.0f ) ;
+    	fadeOut = new AlphaAnimation( 1.0f , 0.0f ) ; 
+    	
+        fadeIn.setInterpolator(new DecelerateInterpolator()); //add this
+        fadeIn.setDuration(1000);
+        fadeOut.setInterpolator(new AccelerateInterpolator()); //and this
+        fadeOut.setStartOffset(1000);
+        fadeOut.setDuration(1000);
+    	mHandler.removeCallbacks(characterAdder);
+    	mHandler.postDelayed(characterAdder, mDelay);
+    	animation = new AnimationSet(false);
+    	animation.addAnimation(fadeIn);
+    	animation.addAnimation(fadeOut);
     }
     
     public void setTypeface(String path){
