d57a0f779efe5be9e6210f8046919f48da6fc816
renlinx007 <mail3renith@gmail.com>
2014-10-31 19:21:44 +0530

    Updated TypeFaceCache

--- TypeFaceTextView/com/nubicz/customviews/TypeFaceCache.java
+++ TypeFaceTextView/com/nubicz/customviews/TypeFaceCache.java
@@ -1,4 +1,17 @@
-class TypeFaceCache{
-  private HashMap<String,TypeFace>
+package com.nubicz.customviews;
 
+import java.util.HashMap;
+
+import android.graphics.Typeface;
+
+
+public class TypeFaceCache {
+	
+  private static HashMap<String,Typeface> pCache = null;
+  
+  public static Typeface getFont(1String path){
+	  if(pCache == null){
+		  
+	  }
+  }
 }
